dc1669618f13bf059503bfecd8c9f933282feb3d
Maël Catteau <mael.catteau@gmail.com>
2025-05-11 17:14:23 +0000

    [REFACTOR][BASE] Ajout d'un préfixe sur les champs crée

--- custom-addons/multibikes_base/models/product_template.py
+++ custom-addons/multibikes_base/models/product_template.py
@@ -6,10 +6,10 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
      # Champ pour la caution
-    caution = fields.Monetary(string='Caution', currency_field='currency_id', help="Montant de la caution à payer pour la location de ce produit.")
+    mb_caution = fields.Monetary(string='Caution', currency_field='currency_id', help="Montant de la caution à payer pour la location de ce produit.")
     
     # Champ pour la valeur en cas de vol
-    value_in_case_of_theft = fields.Monetary(string='Valeur en cas de vol', currency_field='currency_id', help="Valeur du produit en cas de vol.")
+    mb_value_in_case_of_theft = fields.Monetary(string='Valeur en cas de vol', currency_field='currency_id', help="Valeur du produit en cas de vol.")
     
     # Champ pour la devise
-    currency_id = fields.Many2one('res.currency', string='Devise', required=True, default=lambda self: self.env.company.currency_id)
+    mb_currency_id = fields.Many2one('res.currency', string='Devise', required=True, default=lambda self: self.env.company.currency_id)
--- custom-addons/multibikes_base/models/res_partner.py
+++ custom-addons/multibikes_base/models/res_partner.py
@@ -3,12 +3,12 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    x_partner_nationalite = fields.Char(
+    mb_nationalite = fields.Char(
         string='Nationalité du client',
         help="Permet de renseigner la nationalité du client"
     )
 
-    x_partner_n_id = fields.Char(
+    mb_n_id = fields.Char(
         string='Numéro de la carte d\'identité',
         help="Permet de renseigner le numéro de la carte d'identité du client"
     )
