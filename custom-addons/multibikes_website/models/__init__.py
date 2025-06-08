7d222af6c1c8ee80d345b27b75441650a70ae3c7
Maël Catteau <mael.catteau@gmail.com>
2025-06-08 20:36:29 +0000

    [REFACTOR][WEBSITE] Supprime les modèles res_company et res_config_settings non utilisés

--- custom-addons/multibikes_website/models/__init__.py
+++ custom-addons/multibikes_website/models/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """Imports for multibikes_website module."""
-from . import res_company
 from . import mb_renting_period
 from . import mb_renting_stock_period_config
 from . import mb_renting_day_config
-from . import res_config_settings
 from . import product_pricing
 from . import product_template
 from . import product_product
