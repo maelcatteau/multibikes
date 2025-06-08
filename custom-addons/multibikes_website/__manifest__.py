67fd4876f4d1431651262db27d4c1de026e559ad
Maël Catteau <mael.catteau@gmail.com>
2025-06-08 10:18:15 +0000

    [FIX][WEBSITE] Correction de l'import des menus de saison

--- custom-addons/multibikes_website/__manifest__.py
+++ custom-addons/multibikes_website/__manifest__.py
@@ -6,6 +6,9 @@
     "version": "18.0.1.0.0",
     "category": "Sales/Rental",
     "depends": [
+        "sale_renting",
+        "sale_stock",
+        "sale_management",
         "web",
         "website_sale",
         "website_sale_renting",
