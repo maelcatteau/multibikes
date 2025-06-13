567a10f14579d506879a6ff392e98ec1b78ad6d9
Maël Catteau <mael.catteau@gmail.com>
2025-06-13 15:57:16 +0200

    [FEAT][BASE] Ajoute édition directe des infos client (téléphone, email, langue) dans les commandes

--- custom-addons/multibikes_base/models/sale_order.py
+++ custom-addons/multibikes_base/models/sale_order.py
@@ -14,11 +14,29 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
+    # Champs related modifiables pour les informations du partenaire
     partner_phone = fields.Char(
         string="Téléphone client",
         related='partner_id.mobile',
-        readonly=True,
-        store=False
+        readonly=False,
+        store=False,
+        help="Numéro de téléphone du client"
+    )
+    
+    partner_email = fields.Char(
+        string="Email client",
+        related='partner_id.email',
+        readonly=False,
+        store=False,
+        help="Adresse email du client"
+    )
+    
+    partner_lang = fields.Selection(
+        string="Langue client",
+        related='partner_id.lang',
+        readonly=False,
+        store=False,
+        help="Langue préférée du client"
     )
     
     mb_type_de_caution = fields.Selection(
@@ -58,3 +76,29 @@ class SaleOrder(models.Model):
             order.mb_caution_total = sum(
                 line.mb_caution_subtotal or 0.0 for line in order.order_line
             )
+
+    @api.onchange('partner_phone')
+    def _onchange_partner_phone(self):
+        """Synchronise le téléphone avec le partenaire"""
+        if self.partner_id and self.partner_phone:
+            self.partner_id.phone = self.partner_phone
+    
+    @api.onchange('partner_email')
+    def _onchange_partner_email(self):
+        """Synchronise l'email avec le partenaire"""
+        if self.partner_id and self.partner_email:
+            self.partner_id.email = self.partner_email
+    
+    @api.onchange('partner_lang')
+    def _onchange_partner_lang(self):
+        """Synchronise la langue avec le partenaire"""
+        if self.partner_id and self.partner_lang:
+            self.partner_id.lang = self.partner_lang
+    
+    @api.onchange('partner_id')
+    def _onchange_partner_id_custom(self):
+        """Met à jour les champs quand le partenaire change"""
+        if self.partner_id:
+            self.partner_phone = self.partner_id.phone
+            self.partner_email = self.partner_id.email
+            self.partner_lang = self.partner_id.lang
